023489fd52fe2853b2230f5ca5d18e7f4349cbbd
NightDev701 <lukasbaehr01@gmail.com>
2026-05-24 19:06:21 +0200

    interaction (and icon test; not working currently)

--- cmd/scorpionflow/core.go
+++ cmd/scorpionflow/core.go
@@ -1,18 +1,44 @@
 package main
 
 import (
+	_ "embed"
+	"fmt"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
-func main() {
+//go:embed assets/icon.png
+var icon []byte
 
+func main() {
 	a := app.New()
 	w := a.NewWindow("ScorpionFlow")
 
+	// Label, das später geändert werden kann
+	output := widget.NewLabel("Noch keine Nachricht")
+
+	// Bild
+	img := canvas.NewImageFromResource(fyne.NewStaticResource("icon.png", icon))
+	img.FillMode = canvas.ImageFillContain
+
+	// Button
+	btn := widget.NewButton("Drück mich", func() {
+		fmt.Println("Es wurde ein Knopf gedrückt!")
+	})
+
+	// Layout
+	w.SetContent(container.NewVBox(
+		widget.NewLabel("Hallo Welt"),
+		img,
+		btn,
+		output,
+	))
+
 	w.CenterOnScreen()
 	w.Resize(fyne.NewSize(800, 600))
-	w.SetContent(widget.NewLabel("Hallo Welt"))
 	w.ShowAndRun()
 }
